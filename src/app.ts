aa35e0d18ac0be20970069117a7daa0c319fd4ed
Matías Capuano <maticapuano97@gmail.com>
2020-11-04 13:39:18 -0600

    Implemented awilix ioc.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import { loadControllers } from "awilix-express";
+import express, { Application } from "express";
+import loadContainer from "./container";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ work: true });
-});
+//Container
+loadContainer(app);
+
+//Load controllers
+app.use("/api", loadControllers("controllers/*.controller.ts", { cwd: __dirname }));
 
 export { app };
