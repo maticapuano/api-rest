685615068b0a51f12ea32778cc41ba99088bfe5e
Matías Capuano <maticapuano97@gmail.com>
2020-11-04 13:17:46 -0600

    Added container ioc.

---
+++ src/container.ts
@@ -0,0 +1,10 @@
+import { asClass, createContainer } from "awilix";
+import { TestService } from "./services/test.service";
+
+const container = createContainer();
+
+container.register({
+  testService: asClass(TestService).scoped(),
+});
+
+export { container };
