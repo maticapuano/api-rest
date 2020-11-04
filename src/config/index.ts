d195c161912d6cea5fd332c96d771422f63cd194
Matías Capuano <maticapuano97@gmail.com>
2020-11-04 14:35:04 -0600

    Added env database.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,9 @@
 export const environment = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT || 3000;
+
+export const DB_CONNECTION = {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DATABASE: process.env.DB_DATABASE,
+};
